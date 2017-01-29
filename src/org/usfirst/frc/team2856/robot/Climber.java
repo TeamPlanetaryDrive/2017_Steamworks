75d331c6f794ed532103ecac4d098b0988ddda3a
Anorak <Anorak@DESKTOP-PFPPV3M>
2017-01-29 14:19:51 -0500

    Added climber code with basic motor turning functions

--- src/org/usfirst/frc/team2856/robot/Climber.java
+++ src/org/usfirst/frc/team2856/robot/Climber.java
@@ -1,5 +1,28 @@
 package org.usfirst.frc.team2856.robot;
 
-public class Climber {
+import edu.wpi.first.wpilibj.*;
 
+public class Climber {
+	//PMVs
+	private SpeedController motor;
+	private double speed = 0.6;
+	
+	//constructor
+	public Climber(){
+		motor=new Talon(786856); //not actual channel
+	}
+	
+	//where it actually moves, tell it whether or not to move
+	public void set(boolean turn){
+		if(turn)
+			motor.set(speed);
+		else
+			motor.set(0);
+	}
+	
+	//getter
+	public double getSpeed(){return speed;}
+	
+	//setter
+	public void setSpeed(double num){speed=num;}
 }
