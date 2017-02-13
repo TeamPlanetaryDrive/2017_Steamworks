b6430a8b73673fbdf482860dddd8d9fd1d4f019f
ericcao119 <eric7252000@gmail.com>
2017-02-13 17:42:09 -0500

    Revert "Added code so the trigger functions."
    This reverts commit e5a32a8eb4a73f42cf500b1f23174202d1e3d474.

--- src/org/usfirst/frc/team2856/robot/Shooter.java
+++ src/org/usfirst/frc/team2856/robot/Shooter.java
@@ -12,62 +12,55 @@ import edu.wpi.first.wpilibj.Talon;
  * Contains both Shooter and Hopper
  */
 public class Shooter {
-
+	
 	//booleans added
 	boolean shooterOn, hopperOn;
-
+	
 	private static double Kp = 1,
 			Ki = 0.1,
 			Kd = 0;
-
+	
 	SpeedController hopper, shooter;
 	Encoder enc;
 	PIDMotor pidShoot;
-
+	
 	public Shooter() {
-
+		
 		shooterOn = false;
 		hopperOn = false;
-
+		
 		hopper = Constants.hopper;
 		shooter = Constants.shooter;
 		enc = Constants.SEnc;
-
+		
 		pidShoot = new PIDMotor(Kp, Ki, Kd);
-
+		
 		pidShoot.init(shooter, false, enc);
 	}
-
+	
 	public void toggleShooter() {
 		shooterOn = !shooterOn;
 	}
-
+	
 	public void toggleHopper() {
 		hopperOn = !hopperOn;
 	}
-
+	
 	/**
 	 * This is a function that is called in a loop.
 	 * Each call will be a single call within the loop.
 	 * 
 	 */
 	public void updateTele(){
-		//trigger code
-		if(Constants.leftJoystick.getRawButton(1)==true){
-			toggleShooter();
-			toggleHopper();
-		}
-		else{
-			shooterOn=false;
-			hopperOn=false;
-		}
+		
+		
+		
 		if(shooterOn)
 			pidShoot.setSetpoint(0.9);
 		else
 			pidShoot.setSetpoint(0);
-
+		
 		//XXX
-
 		if(hopperOn){
 			if(Constants.leftJoystick.getRawButton(3024721)){
 				Constants.hopper.set(0.5);
@@ -77,9 +70,9 @@ public class Shooter {
 		}else{
 			Constants.hopper.set(0);
 		}
-
+		
 		//Should use PID
-
+		
 	}
-
+	
 }
